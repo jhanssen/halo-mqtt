4793e1c62a6cc17784d48faa9e263b3d5f4172e1
Jan Erik Hanssen <jhanssen@gmail.com>
2023-01-06 20:31:29 -0800

    mostly works

--- src/halo.js
+++ src/halo.js
@@ -1,18 +1,92 @@
+// most of this is ported from https://github.com/nayaverdier/halohome/blob/main/halohome/__init__.py
+
 import needle from "needle";
 import { createBluetooth } from "node-ble";
-import { generate_key } from "./crypto.js";
+import { generate_key, make_packet, random_seq } from "./crypto.js";
 
 const defaultHost = "https://api.avi-on.com";
+const MaxTries = 3;
 
 const data = {};
 
-class Device {
+export class Device {
     constructor(did, pid, name, mac, key) {
         this.did = did;
         this.pid = pid;
         this.name = name;
-        this.mac = mac;
+        this.mac = mac.replace(/(.{2})/g,"$1:").slice(0, -1).toUpperCase();
         this.key = key;
+    }
+
+    async init() {
+        console.log("initializing", this.name, this.mac);
+        const bdev = await data.adapter.waitDevice(this.mac);
+        await bdev.connect();
+        const gatt = await bdev.gatt();
+
+        const SERVICE = "0000fef1-0000-1000-8000-00805f9b34fb";
+        const CHARACTERISTIC_LOW = "c4edc000-9daf-11e3-8003-00025b000b00";
+        const CHARACTERISTIC_HIGH = "c4edc000-9daf-11e3-8004-00025b000b00";
+
+        const service = await gatt.getPrimaryService(SERVICE);
+        const characteristicLow = await service.getCharacteristic(CHARACTERISTIC_LOW);
+        const characteristicHigh = await service.getCharacteristic(CHARACTERISTIC_HIGH);
+
+        this.characteristicLow = characteristicLow;
+        this.characteristicHigh = characteristicHigh;
+
+        console.log("- initialized");
+    }
+
+    async sendPacket(packet) {
+        const csrpacket = make_packet(this.key, random_seq(), packet);
+        const low = csrpacket.slice(0, 20);
+        const high = csrpacket.slice(20);
+
+        // console.log(low);
+        // console.log(high);
+
+        for (let i = 0; i < MaxTries; ++i) {
+            try {
+                if (this.characteristicLow === undefined) {
+                    await this.init();
+                }
+
+                await this.characteristicLow.writeValueWithoutResponse(low);
+                await this.characteristicHigh.writeValueWithoutResponse(high);
+
+                return true;
+            } catch (e) {
+                this.characteristicLow = undefined;
+                this.characteristicHigh = undefined;
+
+                if (i === MaxTries - 1) {
+                    throw e;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    async set_brightness(brightness) {
+        const packet = Buffer.from([
+            0x80 + this.did, 0x80, 0x73, 0, 0x0A,
+            0, 0, 0, brightness, 0, 0, 0, 0
+        ]);
+        // console.log("brighty", packet);
+        return await this.sendPacket(packet);
+    }
+
+    async set_color_temp(color) {
+        const colorBytes = Buffer.allocUnsafe(2);
+        colorBytes.writeInt16BE(color);
+        const packet = Buffer.from([
+            0x80 + this.did, 0x80, 0x73, 0, 0x1D,
+            0, 0, 0, 0x01, colorBytes[0], colorBytes[1], 0, 0
+        ]);
+        // console.log("tempy", packet);
+        return await this.sendPacket(packet);
     }
 }
 
@@ -49,6 +123,16 @@ async function load_devices(host, location_id, key) {
         const mac = adev.friendly_mac_address;
 
         const dev = new Device(devId, pid, name, mac, key);
+        for (let i = 0; i < MaxTries; ++i) {
+            try {
+                await dev.init();
+                break;
+            } catch (e) {
+                if (e.type !== "org.bluez.Error.Failed" || i == MaxTries - 1) {
+                    throw e;
+                }
+            }
+        }
         devs.push(dev);
     }
     return devs;
@@ -64,18 +148,18 @@ async function load_locations(host) {
     for (const loc of resp.locations) {
         const location_id = loc.id;
 
-        const key = generate_key(Buffer.concat(
+        const key = generate_key(Buffer.concat([
             Buffer.from(loc.passphrase, "ascii"),
             Buffer.from([ 0x00, 0x4d, 0x43, 0x50 ])
-        ));
+        ]));
 
         const devices = await load_devices(host, location_id, key);
-        locs.push({ location_id, passphrase: loc.passphrase, devices });
+        locs.push({ location_id, name: loc.name, passphrase: loc.passphrase, devices });
     }
     return locs;
 }
 
-export async function list_devices(email, password, host) {
+export async function list_locations(email, password, host) {
     if (host === undefined)
         host = defaultHost;
     const resp = await make_request(host, "sessions", { email, password });
@@ -86,6 +170,14 @@ export async function list_devices(email, password, host) {
 
     const { bluetooth, destroy } = createBluetooth();
     const adapter = await bluetooth.defaultAdapter();
+    if (!await adapter.isDiscovering())
+        await adapter.startDiscovery();
+
+    if (data.adapter === undefined) {
+        process.on("exit", () => {
+            destroy();
+        });
+    }
 
     data.adapter = adapter;
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,143 @@
 import options from "@jhanssen/options";
-import { list_devices } from "./halo.js";
+import mqtt from "mqtt";
+
+import { list_locations } from "./halo.js";
 
 const option = options("halo-mqtt");
 
-const email = option("email");
-const password = option("password");
-const host = option("host");
+const haloEmail = option("halo-email");
+const haloPassword = option("halo-password");
+const haloHost = option("halo-host");
 
-if (email === undefined || password === undefined) {
+const mqttUser = option("mqtt-user");
+const mqttPassword = option("mqtt-password");
+const mqttHost = option("mqtt-host");
+
+const data = {};
+
+if (mqttHost === undefined) {
+    console.error("need an mqtt host");
+    process.exit(1);
+}
+
+if (haloEmail === undefined || haloPassword === undefined) {
     console.error("need an email and password");
     process.exit(1);
 }
 
+const mqttOpts = {};
+if (mqttUser !== undefined) {
+    mqttOpts.username = mqttUser;
+}
+if (mqttPassword !== undefined) {
+    mqttOpts.password = mqttPassword;
+}
+
+const SetTopic = "/halo-mqtt/set-value";
+const GetTopic = "/halo-mqtt/get-devices";
+
+const client = mqtt.connect(mqttHost, mqttOpts);
+client.on("connect", () => {
+    client.subscribe([SetTopic, GetTopic], () => {
+        console.log("listening for set-value and get-devices");
+    });
+    publishDevices();
+});
+client.on("message", (topic, payload) => {
+    // console.log("topic msg", topic);
+    if (data.locations === undefined) {
+        console.error("no locations");
+        return;
+    }
+    if (topic === SetTopic) {
+        let json;
+        try {
+            json = JSON.parse(payload.toString());
+        } catch (e) {
+            console.error("json parse error", e.message, payload.toString());
+            return;
+        }
+        const locId = json.locId;
+        const devId = json.devId;
+        if (typeof locId !== "number" || typeof devId !== "number") {
+            console.error("invalid id for message");
+            return;
+        }
+        const value = json.value;
+        if (typeof value !== "number") {
+            console.error("invalid value for message");
+            return;
+        }
+
+        // find device
+        let dev = undefined;
+        for (const cloc of data.locations) {
+            if (cloc.location_id === locId) {
+                for (const cdev of cloc.devices) {
+                    if (cdev.did === devId) {
+                        dev = cdev;
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (dev === undefined) {
+            console.error("couldn't find device for message");
+            return;
+        }
+
+        switch (json.type) {
+        case "set-brightness":
+            dev.set_brightness(value).catch(e => {
+                console.error("failed to set brightness", e);
+            });
+            break;
+        case "set-color-temp":
+            dev.set_color_temp(value).catch(e => {
+                console.error("failed to set color temp", e);
+            });
+            break;
+        default:
+            console.error(`invalid data type "${json.type}"`);
+            break;
+        }
+    } else if (topic === GetTopic) {
+        publishDevices();
+    }
+});
+
+function publishDevices() {
+    if (data.locations === undefined)
+        return;
+    const msg = [];
+    for (const loc of data.locations) {
+        const devices = [];
+        msg.push({
+            id: loc.location_id,
+            name: loc.name,
+            devices
+        });
+        for (const dev of loc.devices) {
+            devices.push({
+                id: dev.did,
+                name: dev.name,
+                mac: dev.mac
+            });
+        }
+    }
+    client.publish("/halo-mqtt/devices", JSON.stringify(msg));
+}
+
 async function init() {
-    await list_devices(email, password, host);
+    const locs = await list_locations(haloEmail, haloPassword, haloHost);
+    data.locations = locs;
+    publishDevices();
 }
 
 (async function() {
     await init();
-})().then(() => {
-    process.exit(0);
-}).catch((e) => {
+})().catch((e) => {
     console.error(e);
     process.exit(1);
 });
