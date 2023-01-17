dbf0879016847651f991d091da79b85e3185c25b
Jan Erik Hanssen <jhanssen@gmail.com>
2023-01-16 17:28:20 -0800

    better typings for needle response body

--- src/cloud.ts
+++ src/cloud.ts
@@ -21,7 +21,7 @@ export interface CloudLocation {
     devices: CloudDevice[];
 }
 
-async function make_request(host: string, path: string, body?: KeyValue) {
+async function make_request<T>(host: string, path: string, body?: KeyValue): Promise<T> {
     const headers: { [ key: string ]: string } = {};
     if (data.auth_token !== undefined) {
         headers["Accept"] = "application/api.avi-on.v2";
@@ -34,17 +34,32 @@ async function make_request(host: string, path: string, body?: KeyValue) {
         resp = await needle("get", `${host}/${path}`, { headers });
     }
     // console.log(headers, resp);
-    if (resp.statusCode !== undefined && resp.statusCode >= 200 && resp.statusCode < 300) {
-        return resp.body;
+    if (resp.statusCode !== undefined && resp.statusCode >= 200 && resp.statusCode < 300 && typeof resp.body === "object") {
+        return resp.body as T;
     }
-    throw new Error(`Invalid statusCode ${resp.statusCode} for ${host}/${path}`);
+    throw new Error(`Invalid response, status code: ${resp.statusCode}, body type: ${typeof resp.body} for ${host}/${path}`);
 }
 
-async function load_devices(host: string, locid: string): Promise<CloudDevice[]> {
-    const resp = await make_request(host, `locations/${locid}/abstract_devices`);
+interface ApiAbstractDevice {
+    avid?: number;
+    did?: number;
+    pid?: number;
+    name?: string;
+    friendly_mac_address?: string;
+}
+
+interface ApiAbstractDevicesResponse {
+    abstract_devices?: ApiAbstractDevice[];
+}
+
+async function load_devices(host: string, locid: number): Promise<CloudDevice[]> {
+    const resp = await make_request<ApiAbstractDevicesResponse>(host, `locations/${locid}/abstract_devices`);
     // console.log(resp);
     let startId: number | undefined;
     const devs: CloudDevice[] = [];
+    if (!(resp.abstract_devices instanceof Array)) {
+        throw new Error("No abstract_devices in device response");
+    }
     for (const adev of resp.abstract_devices) {
         if (startId === undefined)
             startId = adev.avid as number;
@@ -58,15 +73,25 @@ async function load_devices(host: string, locid: string): Promise<CloudDevice[]>
     return devs;
 }
 
+interface ApiLocation {
+    id?: number;
+    name?: string;
+    passphrase?: string;
+}
+
+interface ApiLocationsResponse {
+    locations?: ApiLocation[];
+}
+
 async function load_locations(host: string): Promise<CloudLocation[]> {
-    const resp = await make_request(host, "locations");
+    const resp = await make_request<ApiLocationsResponse>(host, "locations");
     // console.log(resp);
-    if (!("locations" in resp)) {
+    if (!(resp.locations instanceof Array)) {
         throw new Error("No locations in locations response");
     }
     const locs: CloudLocation[] = [];
     for (const loc of resp.locations) {
-        const devices = await load_devices(host, loc.id as string);
+        const devices = await load_devices(host, loc.id as number);
         locs.push({
             id: loc.id as number,
             name: loc.name as string,
@@ -77,10 +102,18 @@ async function load_locations(host: string): Promise<CloudLocation[]> {
     return locs;
 }
 
+interface ApiCredentials {
+    auth_token?: string;
+}
+
+interface ApiSessionsResponse {
+    credentials?: ApiCredentials;
+}
+
 export async function load(email: string, password: string, host: string): Promise<CloudLocation[]> {
-    const resp = await make_request(host, "sessions", { email, password });
-    if (!("credentials" in resp)) {
-        throw new Error("No credentials in response");
+    const resp = await make_request<ApiSessionsResponse>(host, "sessions", { email, password });
+    if (typeof resp.credentials !== "object") {
+        throw new Error("No credentials in sessions response");
     }
     data.auth_token = resp.credentials.auth_token;
 
